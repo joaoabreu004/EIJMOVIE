0d4fd96cb4e081a0d3bbe5c2a94210b914ab36a7
João Vitor de Abreu Sousa <jvabreusousa14@gmail.com>
2022-05-23 14:50:17 -0300

    método toggle explicação

--- scripts/menulateral.js
+++ scripts/menulateral.js
@@ -9,6 +9,8 @@ const btnAbrir = buscarElemento('.btn_abrirlateral');
 
 btnAbrir.addEventListener('click', () => {
     menu.classList.toggle('menulateral--ativo');
+    //O método toggle () da interface DOMTokenList remove um token existente da lista e 
+    //retorna false. Se o token não existir, ele será adicionado e a função retornará verdadeira.
 })
 
 
