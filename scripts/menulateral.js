8517e42a63c43ca893df75b59eee78957179e622
João Abreu <jvabreusousa14@gmail.com>
2022-05-23 22:38:20 -0300

    animação commit

--- scripts/menulateral.js
+++ scripts/menulateral.js
@@ -8,6 +8,7 @@ const btnAbrir = buscarElemento('.btn_abrirlateral');
 
 
 btnAbrir.addEventListener('click', () => {
+    btnAbrir.classList.toggle('active');
     menu.classList.toggle('menulateral--ativo');
     //O método toggle () da interface DOMTokenList remove um token existente da lista e 
     //retorna false. Se o token não existir, ele será adicionado e a função retornará verdadeira.
