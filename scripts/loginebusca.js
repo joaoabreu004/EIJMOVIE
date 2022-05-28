64fd3b240ad6a09f7426f1a1807795b2156fe98b
João Vitor de Abreu Sousa <jvabreusousa14@gmail.com>
2022-05-28 09:30:21 -0300

    Login pesquisa

--- scripts/loginebusca.js
+++ scripts/loginebusca.js
@@ -1,13 +1,9 @@
-const btnLogin = buscarElemento('.button_login'); 
-const login = buscarElemento('.login')
 
-function abrirMenu(elemento){
-    if(elemento.style.display == "none"){
-        elemento.style.display = "flex"; 
-    } else {
-        fecharMenu(elemento);
-    }
-}    
+const btnLogin = buscarElemento('.button_login'); 
+const btnPesquisar = buscarElemento('.button_pesquisar'); 
+const login = buscarElemento('.login')
+const pesquisar = buscarElemento('.pesquisar');
+
 
 function fecharMenu(elemento){
     if(elemento.style.display == "flex"){
@@ -15,6 +11,21 @@ function fecharMenu(elemento){
     }
 }
 
+function abrirMenu(elemento){
+    if(elemento.style.display == "none"){
+        elemento.style.display = "flex"; 
+    } else {
+        fecharMenu(elemento);
+    }
+}  
+
+
 btnLogin.addEventListener('click', () => {
     abrirMenu(login);
+    fecharMenu(pesquisar);
+})
+
+btnPesquisar.addEventListener('click', () => {
+    abrirMenu(pesquisar);
+    fecharMenu(login);
 })
